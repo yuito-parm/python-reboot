9908f58964be8618411fab78de6dbec5cb98b867
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-07 11:49:40 +0900

    点数の数も自由にできる成績アプリを追加

---
+++ score_input_variable_subjects.py
@@ -0,0 +1,33 @@
+def list_add(scores):
+    result = 0
+    for num in scores:
+        result += num
+    
+    return result
+
+def list_average(scores):
+    result = 0
+    for score in scores:
+        result += score
+    
+    average = result / len(scores)
+    return average
+
+people = []
+while True:
+    person = {}
+    scores = []
+    person["name"] = input("名前を入力してください：")
+    if person["name"] == "exit":
+        break
+    sbj = int(input("何教科ありますか？"))
+    for i in range(sbj):
+        scores.append(int(input(f"{i + 1}つめの点数を入力してください。")))
+    person["scores"] = scores
+    people.append(person)
+    print(person)
+
+for person in people:
+    add = list_add(person["scores"])
+    average = list_average(person["scores"])
+    print(f"{person['name']}さんの合計は{add}点、平均は{average:.1f}点です。")
